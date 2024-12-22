8b841fbae84b10690ff2e76fe3720f7e8463e5f8
YX <yxiang@local.attlocal.net>
2024-12-22 10:21:11 -0800

    d17

---
+++ go/y2024/d17/solution.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"adventofcode/utils"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+type solution struct {
+	ans int
+}
+
+func (s *solution) run1() {
+}
+
+func (s *solution) run2() {
+}
+
+func (s *solution) res() int {
+	return s.ans
+}
+
+func buildSolution(r io.Reader) *solution {
+	lines, err := utils.LinesFromReader(r)
+	if err != nil {
+		log.Fatalf("could not read input: %v %v", lines, err)
+	}
+
+	return &solution{
+		ans: 0,
+	}
+}
+
+func part1(r io.Reader) int {
+	s := buildSolution(r)
+	s.run1()
+	return s.res()
+}
+
+func part2(r io.Reader) int {
+	s := buildSolution(r)
+	s.run2()
+	return s.res()
+}
+
+func main2() {
+	Input, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatalf("fail open input.txt %v", err)
+	}
+	defer Input.Close()
+	fmt.Println("p1 res 🙆-> ", part1(Input))
+	fmt.Println("p2 res 🙆-> ", part2(Input))
+}
